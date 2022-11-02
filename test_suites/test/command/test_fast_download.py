15a3ee9d5eff274feb978205d2fb02fe59fcc73d
ghsnapple-sunshine <56648406+ghsnapple-sunshine@users.noreply.github.com>
2022-11-02 10:03:38 +0800

    [#16][重构]增加了一个Test方法

---
+++ test_suites/test/command/test_fast_download.py
@@ -0,0 +1,24 @@
+import logging
+
+from buffett.common.pendelum import DateTime
+from buffett.task import StockListTask
+from test import Tester
+
+
+class FastDownloadTest(Tester):
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_download_fast(self):
+        """
+        download的镜像测试（股票清单、不循环）
+
+        :return:
+        """
+        logging.basicConfig(level=logging.INFO)
+        now = DateTime.now()
+        operator = self.operator
+
+        tasks = [StockListTask(operator=operator, start_time=now)]
+        for task in tasks:
+            task.run()
