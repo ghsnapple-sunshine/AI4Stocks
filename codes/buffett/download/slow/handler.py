866cd65fdb27424a7ce82286eb74dc70ed94d319
ghsnapple-sunshine <56648406+ghsnapple-sunshine@users.noreply.github.com>
2022-11-04 21:21:01 +0800

    [#14][#15]DailyHandler及MinuteHandler性能优化
    1. 通过表格拼接方式，减少重复查找记录是否存在的开销。

--- codes/buffett/download/slow/handler.py
+++ codes/buffett/download/slow/handler.py
@@ -73,23 +73,14 @@ class SlowHandler(Handler, TableName):
                           para: Para,
                           record: DataFrame,
                           table_name: str) -> None:
-        '''
-        cur_start_time = DateFactory.create(record[START_DATE].iloc[0])  # datetime类型在DateFrame会被转为Timestamp
-        cur_end_time = DateFactory.create(record[END_DATE].iloc[0])
-        if para.span.start < cur_start_time:
-            spara = para.clone().with_end(cur_start_time - self._freq.to_duration())
-            self._download_and_save_a_stock(para=spara, table_name=table_name)
-        else:
-            para.with_start(start=cur_start_time)
-
-        if para.span.end > cur_end_time:  # 注意无需elif
-            spara = para.clone().with_start(cur_end_time + self._freq.to_duration())
-            self._download_and_save_a_stock(para=spara, table_name=table_name)
-        else:
-            para.with_end(end=cur_end_time)
-        '''
         done_span = DateSpan(record[START_DATE].iloc[0], record[END_DATE].iloc[0])
         todo_span_ls = para.span.subtract(done_span)
+        """
+        logging.info('Should download {0} for {1} {2} {3}'.format(todo_span_ls,
+                                                                  para.comb.freq,
+                                                                  para.stock.code,
+                                                                  para.comb.fuquan))
+        """
         for span in todo_span_ls:
             self._download_and_save_a_stock(para=para.clone().with_span(span=span),
                                             table_name=table_name)
@@ -98,6 +89,12 @@ class SlowHandler(Handler, TableName):
     def _download_and_save_a_stock(self,
                                    para: Para,
                                    table_name: str) -> None:
+        """
+        logging.info('Should download {0} for {1} {2} {3}'.format(para.span,
+                                                                  para.comb.freq,
+                                                                  para.stock.code,
+                                                                  para.comb.fuquan))
+        """
         data = self._download(para=para)
         self._save_to_database(table_name=table_name, df=data)
 
@@ -127,3 +124,156 @@ class SlowHandler(Handler, TableName):
                          (records[SOURCE] == para.comb.source)]
         return record
 # endregion
+
+
+'''
+import logging
+from abc import abstractmethod
+
+import pandas as pd
+from pandas import DataFrame, Series
+
+from buffett.common.error import ParamTypeError
+from buffett.common.pendelum import DateSpan, Date
+from buffett.common.tools import dataframe_not_valid
+from buffett.constants.col import FREQ, FUQUAN, SOURCE, START_DATE, END_DATE
+from buffett.constants.col.stock import CODE
+from buffett.download.fast.stock_list_handler import StockListHandler as SHandler
+from buffett.download.handler import Handler
+from buffett.download.mysql import Operator
+from buffett.download.para import Para
+from buffett.download.slow.recorder import DownloadRecorder as Recorder
+from buffett.download.slow.table_name import TableName
+from buffett.download.types import FreqType, SourceType, FuquanType
+
+_TDRCD_START, _TDRCD_END = 'todo_record_start', 'todo_record_end'
+_DORCD_START, _DORCD_END = 'done_record_start', 'done_record_end'
+
+
+class SlowHandler(Handler, TableName):
+    """
+    实现多张表的下载，存储
+    """
+
+    def __init__(self, operator: Operator):
+        super(SlowHandler, self).__init__(operator=operator)
+        self._recorder = Recorder(operator=operator)
+        self._source = SourceType.AKSHARE_DONGCAI
+        self._fuquans = [FuquanType.BFQ]
+        self._freq = FreqType.DAY
+
+    # region 公共方法
+    def obtain_data(self, para: Para):
+        if not isinstance(para.span, DateSpan):
+            raise ParamTypeError('para.span', DateSpan)
+
+        para = para.clone().with_end(Date.today(), para.span.end > Date.today())
+
+        stock_list = SHandler(self._operator).get_data()
+        done_records = self._recorder.get_data()
+
+        todo_records = self._get_todo_records(stock_list=stock_list,
+                                              done_records=done_records,
+                                              para=para)
+        comb_records = SlowHandler._get_comb_records(todo_records=todo_records,
+                                                     done_records=done_records)
+        tbs = [self._download_n_save_a_stock(row) for index, row in comb_records.iterrows()]
+        return tbs
+
+    @abstractmethod
+    def get_data(self, para: Para) -> DataFrame:
+        pass
+
+    # endregion
+
+    # region 私有方法
+
+    def _get_todo_records(self,
+                          stock_list: DataFrame,
+                          done_records: DataFrame,
+                          para: Para) -> DataFrame:
+        """
+        计算待下载的记录
+
+        :param stock_list:          股票代码
+        :param done_records:        下载记录
+        :param para:                span
+        :return:                    待下载记录
+        """
+        fuquan_df = DataFrame(self._fuquans, columns=[FUQUAN])
+        todo_records = pd.merge(stock_list[[CODE]], fuquan_df, how='cross')
+        todo_records[FREQ] = self._freq
+        todo_records[SOURCE] = self._source
+        todo_records[START_DATE] = para.span.start
+        todo_records[END_DATE] = para.span.end
+        todo_records = pd.concat([todo_records, done_records, done_records]).drop_duplicates(keep=False)
+        return todo_records
+
+    @classmethod
+    def _get_comb_records(cls,
+                          todo_records: DataFrame,
+                          done_records: DataFrame) -> DataFrame:
+        """
+        将待下载记录和已下载记录进行拼接
+
+        :param todo_records:
+        :param done_records:
+        :return:
+        """
+        if dataframe_not_valid(done_records):
+            todo_records[_DORCD_START] = pd.NA
+            todo_records[_DORCD_END] = pd.NA
+            return todo_records
+
+        done_records.rename(columns={START_DATE: _DORCD_START,
+                                     END_DATE: _DORCD_END}, inplace=True)
+        todo_records = pd.merge(todo_records, done_records, how='left', on=[CODE, FREQ, SOURCE, FUQUAN])
+        return todo_records
+
+    def _download_n_save_a_stock(self,
+                                 row: Series) -> None:
+        para = Para().with_code(row[CODE]) \
+            .with_freq(row[FREQ]) \
+            .with_source(row[SOURCE]) \
+            .with_fuquan(row[FUQUAN])
+
+        todo_span = DateSpan(row[START_DATE], row[END_DATE])
+        done_span = None
+        if pd.isna(row[_DORCD_START]):
+            data = self._download(para=para.with_span(todo_span))
+        else:
+            done_span = DateSpan(row[_DORCD_START], row[_DORCD_END])
+            todo_ls = todo_span.subtract(done_span)
+            data = pd.concat([self._download(para=para.with_span(span)) for span in todo_ls])
+
+        table_name = SlowHandler._get_table_name_by_code(para=para)
+        self._save_to_database(table_name=table_name, df=data)
+        total_span = todo_span if done_span is None else todo_span.add(done_span)
+        self._recorder.save(para=para.with_span(total_span))
+        """
+        logging.info('Should download {0} for {1} {2} {3}'.format(para.span,
+                                                                  para.comb.freq,
+                                                                  para.stock.code,
+                                                                  para.comb.fuquan))
+        """
+
+    @abstractmethod
+    def _download(self, para: Para) -> DataFrame:
+        pass
+
+    @abstractmethod
+    def _save_to_database(self,
+                          table_name: str,
+                          df: DataFrame) -> None:
+        pass
+
+    @classmethod
+    def _log_success_download(cls, para: Para):
+        logging.info('Successfully Download Stock {0} {1} {2} {3}'.format(
+            para.comb.freq,
+            para.stock.code,
+            para.stock.name,
+            para.comb.fuquan))
+
+# endregion
+'''
