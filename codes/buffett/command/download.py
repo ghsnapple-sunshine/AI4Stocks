c1885d57bd33b32d2d0134a3bb2da9f457ff9e74
ghsnapple-sunshine <56648406+ghsnapple-sunshine@users.noreply.github.com>
2022-11-11 11:47:54 +0800

    [#6][Change]StockReformTask纳入TaskScheduler

--- codes/buffett/command/download.py
+++ codes/buffett/command/download.py
@@ -1,19 +1,25 @@
 import logging
 
-from buffett.common.pendelum import DateTime, Duration
-from buffett.download.mysql import RoleType, Operator
-from buffett.task import StockDailyTask, StockListTask, StockMinuteTask, TaskScheduler
+from buffett.common.pendelum import DateTime
+from buffett.download.mysql import Operator
+
+from buffett.download.mysql.types import RoleType
+from buffett.maintain import ReformMaintain
+from buffett.task import TaskScheduler, StockReformTask, StockListTask, StockDailyTask, StockMinuteTask
 
 
 def download():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s.%(msecs)03d [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='## %Y-%m-%d %H:%M:%S')
     now = DateTime.now()
     operator = Operator(RoleType.DbStock)
-    sch = TaskScheduler(
-        operator=operator,
-        tasks=[
-            StockListTask(operator=operator, start_time=now),
-            StockDailyTask(operator=operator, start_time=now + Duration(seconds=1)),
-            StockMinuteTask(operator=operator, start_time=now + Duration(seconds=2))]
-    )
+    """
+    sch = TaskScheduler(operator=operator,
+                        tasks=[StockListTask(operator=operator, start_time=now),
+                               StockDailyTask(operator=operator, start_time=now.add(seconds=1)),
+                               StockMinuteTask(operator=operator, start_time=now.add(seconds=2)),
+                               StockReformTask(operator=operator, start_time=now.add(seconds=3))])
     sch.run()
+    """
+    ReformMaintain(operator=operator).run()
