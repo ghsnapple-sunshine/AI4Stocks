2ed46dad43d8072ada3b5a72826a5c87b2cd82a4
ghsnapple-sunshine <56648406+ghsnapple-sunshine@users.noreply.github.com>
2022-12-09 00:21:41 +0800

    #21 变更: cleanup支持analysis数据库

--- codes/buffett/command/cleanup.py
+++ codes/buffett/command/cleanup.py
@@ -1,21 +1,39 @@
-from typing import Optional
-
 from buffett.adapter.wellknown import tqdm
-from buffett.common.error import ParamTypeError
+from buffett.common.tools import dataframe_not_valid
 from buffett.download.mysql import Operator
 from buffett.download.mysql.types import RoleType
 
 
-def cleanup(like: Optional[str] = None) -> None:
-    c1 = like is None
-    c2 = isinstance(like, str)
-    if not c1 and not c2:
-        raise ParamTypeError("like", Optional[str])
+def get_operator_root():
+    """
+    由于使用root权限需要输入密码，该方法可以避免在操作其他数据库时（前）需要输入root密码，改善体验。
+    :return:
+    """
+    yield Operator(RoleType.ROOT)
 
-    op = Operator(RoleType.ROOT)
+
+operators = {
+    "1": get_operator_root(),
+    "2": Operator(RoleType.DbAnaly),
+}
+
+
+def cleanup() -> None:
+    db = input("请选择要操作的数据库： 1.stocks, 2.analysis. (1/2)")
+    op = operators.get(db)
+    if op is None:
+        print("输入错误。")
+        return
+    #
     tbls = op.execute("show tables", fetch=True)
+    if dataframe_not_valid(tbls):
+        print("数据库为空")
+        return
     tbls = tbls[tbls.columns[0]]
-    to_delete_tbls = tbls[tbls.apply(lambda x: like in x)] if c2 else tbls
+    #
+    like = input("请选择要匹配的表名：如'dc_stock_dayinfo':")
+    to_delete_tbls = tbls[tbls.apply(lambda x: like in x)] if like != "" else tbls
+
     if len(to_delete_tbls) == 0:
         print("没有表格待删除。")
         return
@@ -31,3 +49,7 @@ def cleanup(like: Optional[str] = None) -> None:
         print("删除完毕。")
     else:
         print("跳过删除。")
+
+
+if __name__ == "__main__":
+    cleanup()
